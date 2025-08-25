b93b55d57812d6a0fa2d6b5295d32ec60af6b44d
Rod <69829465+ketraline@users.noreply.github.com>
2025-08-25 04:12:16 +0200

    Update main.lua to reference other files

--- main.lua
+++ main.lua
@@ -1,29 +1,11 @@
-SMODS.Atlas {
-	key = "clownfish",
-	path = "clownfish.png",
-	px = 71,
-	py = 95
-}
+local filepath = SMODS.current_mod.path
 
-SMODS.Joker {
-	key = 'tronek',
-	loc_txt = {
-		name = 'EPIC TRONEK JOKER',
-		text = {
-			"Earn {C:money}$#1#{} (eighty) at",
-			"end of round"
-		}
-	},
-	config = { extra = { money = 80 } },
-	rarity = 3,
-	atlas = 'clownfish',
-	pos = { x = 0, y = 0 },
-	cost = 6,
-	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.money } }
-	end,
-	calc_dollar_bonus = function(self, card)
-		local bonus = card.ability.extra.money
-		if bonus > 0 then return bonus end
+local assets = NFS.getDirectoryItems(filepath .. "lua")
+for _, asset in ipairs(assets) do
+	print("CLOWNFISH - Loading " .. asset)
+	local load, err = SMODS.load_file("lua/" .. asset)
+	if err then
+		error(err) 
 	end
-}
+	load()
+end
